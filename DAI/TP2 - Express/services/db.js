83e305e7b9b7d6578ccfdf2acad2dbef50ad5b47
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-06-09 10:20:34 -0300

    Deleted HashIds

--- DAI/TP2 - Express/services/db.js
+++ DAI/TP2 - Express/services/db.js
@@ -1,6 +1,5 @@
 const { connect } = require("mongoose");
 require("dotenv").config();
-const HashIds = new (require("hashids"))(process.env.HashKey);
 const MongoURL = process.env.MongoURL;
 const Entry = require("../models/Entry");
 (async () => {
@@ -18,16 +17,16 @@ module.exports = class {
         return await Entry.find();
     }
     static async read_entry(id){
-        return await Entry.findById(HashId.decodeHex(id));
+        return await Entry.findById(id);
     }
     static async create_entry(_entry){
-        return HashIds.encodeHex((await Entry.create(_entry)));
+        return (await Entry.create(_entry))._id.toString();
     }
     static async update_entry(update, id){
-        return HashIds.encodeHex(await Entry.findByIdAndUpdate({ _id: HashId.decodeHex(id) }, update, { new: true })._id.toString());
+        return await Entry.findByIdAndUpdate({ _id: id }, update, { new: true });
     }
     static async delete_entry(id){
-        await Entry.deleteOne({ _id: HashId.decodeHex(id) });
+        await Entry.deleteOne({ _id: id });
         return { success: true, message: "Entry deleted" }
     }
 };
